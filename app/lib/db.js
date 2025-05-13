ada623b77f0dea0d88ca9064eab3fb57749854e4
mahammedjunedattar <123448134+mahammedjunedattar@users.noreply.github.com>
2025-05-13 10:25:57 -0700

    Update db.js

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,8 +1,8 @@
 // lib/mongodb.js
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI;
-if (!uri) {
+const uri = process.env.MONGODB_URIS;
+
   throw new Error('MONGODB_URI environment variable is not defined');
 }
 
